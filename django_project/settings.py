7a31b61197568859843107bee1ed57e67f63d562
Brad Conyers <conyerbd@gmail.com>
2016-02-04 21:03:40 -0500

    Added WCPO article and Atumsoft in the news section on press tab.

--- django_project/settings.py
+++ django_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'YCiq6mV3CCSCJHtLjir7mVhlx0pBNmz9qyJ024GGXYE7lTV8Hh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
